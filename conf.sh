1f9c514128115decf009cbedc2fc4ba833c7f94c
Parallels <mattzett@gmail.com>
2021-07-27 14:52:02 -0400

    linux conf.sh

---
+++ conf.sh
@@ -0,0 +1,18 @@
+export LIBS=""
+export FCLAW=~/Projects/forestclaw
+export FCLAW_BIN=$FCLAW/local/bin
+
+./configure \
+    --enable-clawpack \
+    --disable-shared \
+    --without-blas \
+    F77="gfortran" \
+    CC="gcc" \
+    CXX="g++" \
+    LDFLAGS="-lgfortran"
+#    CFLAGS="-Wall -O3 -DFCLAW2D_PATCHDIM=2 -DFCLAW2D_REFINEDIM=2" \
+#    CXXFLAGS="-O3 -Wall -DFCLAW2D_PATCHDIM=2 -DFCLAW2D_REFINEDIM=2" \
+#     FFLAGS=-lgfortran
+#    FFLAGS="-fast -assume buffered_io" 
+#    LIBS="-lmkl_intel_lp64 -lmkl_core -lmkl_sequential"
+#    --enable-clawpack \
